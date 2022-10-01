f6e1599a46942f0154cc363cfd46494dfe5ab295
Volantis <putzstuck@gmail.com>
2022-10-01 17:40:48 -0400

    updated js file to work unless user chooses to cancel

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,50 @@
 // User variables
 var passwordLength;
-
-var lowercase;
-// var uppercase;
-// var numeric;
-// var special;
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var password = document.getElementById("password");
 
-// Write password to the #password input
 function writePassword() {
+  var empty = [];
+  var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]", ";"];
+  var password = "";
+
   // Use this number to determine the length of the password
   let passwordLength = (parseInt(prompt("Enter a number between 8 and 128"))) - 1;
   
-  // Password variables
-  var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var alphaUpper = alpha.map(element => {
-    return element.toUpperCase();
-  });
-  var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]", ";"];
-  // This variable will be a string including all of the characters chosen by the user
-  var allChars = alpha.concat(numeric, specialChars, alphaUpper);
-  var includedChars = allChars.join(""); /*alpha.join(""), numeric.join("");*/
-  var password = "";
-  
   // If lowercase is true, the alpha variable is added to the password array
-  // lowercase = confirm("Include lowercase letters?");
-  // lowercase = alpha
-
+  if (confirm("Include lowercase letters?")) {
+      includedChars1 = empty.concat(alpha).join("");
+      console.log(empty);
+      console.log(includedChars1);
+  }
   // If uppercase is true, the alpha variable is converted to uppercase and is added to the password array
-  // uppercase = confirm("Include uppercase letters?")
-
+  if (confirm("Include uppercase letters?")) {
+      includedChars2 = (empty.concat(alpha.map(element => {
+      return element.toUpperCase()
+      }).join(""))) + includedChars1;
+      console.log(empty);
+      console.log(includedChars2);
+  }
   // If number is true, the numeric variable is added to the password array
-  // number = confirm("Include numbers?")
-
+  if (confirm("Include numbers?")) {
+      includedChars3 = empty.concat(numeric).join("") + includedChars2;
+      console.log(empty);
+      console.log(includedChars3);
+  }
   // If special is true, the characters variable is added to the password array
-  // special = confirm("Include special characters?")
-  
+  if (confirm("Include special characters?")) {
+      includedChars4 = empty.concat(specialChars.join("")) + includedChars3;
+      console.log(empty);
+      console.log(includedChars4);
+  }
+  // For loop to randomize selection of characters
   for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * includedChars.length);
-    password += includedChars.substring(randomNumber, randomNumber +1);
+    var randomNumber = Math.floor(Math.random() * includedChars4.length);
+    password += includedChars4.substring(randomNumber, randomNumber +1);
    }
    document.getElementById("password").value = password;
-   
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
 }
 
 // Add event listener to generate button
