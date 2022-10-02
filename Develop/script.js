caf8c7509ba1cf026c52b1086ecee00bd2e09326
Volantis <putzstuck@gmail.com>
2022-10-02 06:51:17 -0400

    cleaned up js file

--- Develop/script.js
+++ Develop/script.js
@@ -9,17 +9,15 @@ function writePassword() {
   var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "]", ";"];
   var password = "";
-
-  // Use this number to determine the length of the password
+    // Use this number to determine the length of the password
   let passwordLength = (parseInt(prompt("Enter a number between 8 and 128"))) - 1;
-
-  // Creates an alert window if the user chooses a number outside of the given range   
+    // Creates an alert window if the user chooses a number outside of the given range   
   if (passwordLength < 7 || passwordLength >= 128) {
     alert("Please choose a number between 8 and 128!");
     return;
   }
-  
-  // If lowercase is true, the alpha variable is added to the password array
+    // If lowercase is true, the alpha variable is added to the password array
+    // If lowercase is false, the password array is set equal to the empty array     
   if (confirm("Include lowercase letters?")) {
     includedChars1 = empty.concat(alpha).join("");
     console.log(empty);
@@ -29,33 +27,36 @@ function writePassword() {
     console.log(empty);
     console.log(includedChars1);
   }
-  // If uppercase is true, the alpha variable is converted to uppercase and is added to the password array
+    // If uppercase is true, the alpha variable is converted to uppercase and is added to the password array
+    // If uppercase is false, the password array is set equal to the previous array
   if (confirm("Include uppercase letters?")) {
-      includedChars2 = (empty.concat(alpha.map(element => {
-      return element.toUpperCase()
-      }).join(""))) + includedChars1;
-      console.log(empty);
-      console.log(includedChars2);
+    includedChars2 = (empty.concat(alpha.map(element => {
+    return element.toUpperCase()
+    }).join(""))) + includedChars1;
+    console.log(empty);
+    console.log(includedChars2);
   } else {
     includedChars2 = includedChars1;
     console.log(empty);
     console.log(includedChars2);
   }
   // If number is true, the numeric variable is added to the password array
+  // If number is false, the password array is set equal to the previous array
   if (confirm("Include numbers?")) {
-      includedChars3 = empty.concat(numeric).join("") + includedChars2;
-      console.log(empty);
-      console.log(includedChars3);
+    includedChars3 = empty.concat(numeric).join("") + includedChars2;
+    console.log(empty);
+    console.log(includedChars3);
   } else {
     includedChars3 = includedChars2;
     console.log(empty);
     console.log(includedChars3);
   }
   // If special is true, the characters variable is added to the password array
+  // If special is false, the password array is set equal to the previous array
   if (confirm("Include special characters?")) {
-      includedChars4 = empty.concat(specialChars.join("")) + includedChars3;
-      console.log(empty);
-      console.log(includedChars4);
+    includedChars4 = empty.concat(specialChars.join("")) + includedChars3;
+    console.log(empty);
+    console.log(includedChars4);
   } else {
     includedChars4 = includedChars3;
     console.log(empty);
